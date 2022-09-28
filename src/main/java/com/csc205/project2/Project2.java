a10ed2309a25403f4114c242f4f41ca77e649549
drxpadrian <adrian.linares94@gmail.com>
2022-09-27 18:15:29 -0700

    Doneso

--- src/main/java/com/csc205/project2/Project2.java
+++ src/main/java/com/csc205/project2/Project2.java
@@ -1,5 +1,6 @@
 package com.csc205.project2;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +22,7 @@ public class Project2 {
         ThreeDimensionalShape cube = new Cube(5.0);
         ThreeDimensionalShape cylinder = new Cylinder(4.0, 1.0);
 
-        // We'll cover this stuff in module 4
+
         List<ThreeDimensionalShape> shapes = new ArrayList<>();
         shapes.add(sphere);
         shapes.add(cube);
@@ -33,8 +34,22 @@ public class Project2 {
         The following code works with the code given.
          */
 
-        Sphere sphere = new Sphere(2.0);
+
+
+        ThreeDimensionalShape sphere = new Sphere(2.0);
         System.out.println(sphere);
+
+        ThreeDimensionalShape cylinder = new Cylinder(4.0, 1.0);
+        System.out.println(cylinder);
+
+        ThreeDimensionalShape cube = new Cube(5.0);
+        System.out.println(cube);
+
+        List<ThreeDimensionalShape> shapes = new ArrayList<>();
+        shapes.add(sphere);
+        shapes.add(cube);
+        shapes.add(cylinder);
+
     }
 
 }
